b2ae199bb571b59dc031a50ab070f38d58c673c2
dough <doughertypiper@gmail.com>
2017-04-23 20:09:51 -0400

    Tutorial 5 pretty much completed.

--- src/main/java/com/recipe/controllers/RecipeController.java
+++ src/main/java/com/recipe/controllers/RecipeController.java
@@ -1,12 +1,19 @@
 package com.recipe.controllers;
 
+import com.recipe.domains.Ingredient;
+import com.recipe.domains.Recipe;
 import com.recipe.repositories.RecipeRepository;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 /**
@@ -33,5 +40,44 @@ public class RecipeController {
         model.addAttribute("recipe", recipeRepository.findOne(recipeId));
         //Serve the /templates/recipe.html template.
         return "recipe";
+    }
+
+    @GetMapping("/create")
+    public String serveForm(Model model) {
+
+        Recipe recipe = new Recipe();
+        model.addAttribute("recipe", recipe);
+        return "create";
+    }
+
+    @PostMapping(value = "/create", params = {"addRow"})
+    public String addRow(@ModelAttribute("recipe") Recipe recipe, final BindingResult bindingResult) {
+
+        if (recipe.getIngredients() == null) {
+            recipe.setIngredients(new ArrayList<>());
+        }
+
+        recipe.getIngredients().add(new Ingredient());
+        return "create";
+    }
+
+    @PostMapping(value = "/create", params = {"removeRow"})
+    public String removeRow(@ModelAttribute("recipe") Recipe recipe,
+                            final BindingResult bindingResult,
+                            HttpServletRequest request) {
+        Integer rowId = Integer.valueOf(request.getParameter("removeRow"));
+        recipe.getIngredients().remove(rowId.intValue());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String saveRecipe(@ModelAttribute("recipe") Recipe recipe, Model model) {
+
+        for (Ingredient ingredient : recipe.getIngredients()) {
+            ingredient.setRecipe(recipe);
+        }
+
+        recipeRepository.save(recipe);
+        return "redirect:/recipes";
     }
 }
