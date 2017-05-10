927065633f78e01197990adde81483e9ec147e0f
dough <doughertypiper@gmail.com>
2017-05-10 19:32:45 -0400

    Completed userdetails.

---
+++ src/main/java/com/recipe/services/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package com.recipe.services;
+
+import com.recipe.domains.User;
+import com.recipe.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by dough on 5/10/2017.
+ */
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    private final
+    UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("The supplied username does not exist in the database.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new HashSet<>());
+    }
+}
